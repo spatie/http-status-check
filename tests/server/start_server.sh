894e0c269c7bce6f8f48f55538b61c1ee1401ff9
Rod Elias <rodiney@gmail.com>
2018-06-05 04:04:56 -0300

    make sure to always execute npm install (#53)

--- tests/server/start_server.sh
+++ tests/server/start_server.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
+npm install
+
 if [ -z ${TRAVIS_JOB_ID} ]; then
     # not running under travis, stay in foreground until stopped
     node server.js
 else
     cd tests/server
 
-    npm install
     # running under travis, daemonize
     (node server.js &) || /bin/true
 fi
