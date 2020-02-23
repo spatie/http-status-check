6ae63208a30e67d6a94394a27a7940d9eee77cab
James <inbox.dev@jhodges.co.uk>
2020-02-23 20:54:32 +0000

    log redirects history as multiple results

--- src/CrawlLogger.php
+++ src/CrawlLogger.php
@@ -117,9 +117,38 @@ class CrawlLogger extends CrawlObserver
         ResponseInterface $response,
         ?UriInterface $foundOnUrl = null
     ) {
-        $statusCode = $response->getStatusCode();
-        $reason = $response->getReasonPhrase();
-        $this->addResult((String)$url,(string)$foundOnUrl,$statusCode,$reason);
+        // https://github.com/guzzle/guzzle/blob/master/docs/faq.rst#how-can-i-track-redirected-requests
+        if($response->getHeader('X-Guzzle-Redirect-History')){
+            // Retrieve both Redirect History headers
+            $fullRedirectReport = [];
+            // Retrieve both Redirect History headers
+            $redirectUriHistory = $response->getHeader('X-Guzzle-Redirect-History'); // retrieve Redirect URI history
+            $redirectCodeHistory = $response->getHeader('X-Guzzle-Redirect-Status-History'); // retrieve Redirect HTTP Status history
+            // Add the initial URI requested to the (beginning of) URI history
+            array_unshift($redirectUriHistory, (string)$url);
+            // Add the final HTTP status code to the end of HTTP response history
+            array_push($redirectCodeHistory, $response->getStatusCode());
+            $fullRedirectReport = [];
+            foreach ($redirectUriHistory as $key => $value) {
+                $fullRedirectReport[$key] = ['location' => $value, 'code' => $redirectCodeHistory[$key]];
+            }
+
+            foreach($fullRedirectReport as $k=>$redirect){
+                $this->addResult(
+                    (String)$redirect['location'],
+                    (string)$foundOnUrl,
+                    $redirect['code'],
+                    ''
+                );
+            }
+        }else{
+            $this->addResult(
+                (String)$url,
+                (string)$foundOnUrl,
+                $response->getStatusCode(),
+                ''
+            );
+        }
     }
 
     public function crawlFailed(
